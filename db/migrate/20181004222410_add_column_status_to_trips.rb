2aee8ae2a29940627364387578660f7c0b192b48
github username <github email address>
2018-10-04 16:25:17 -0600

    add complete attribute to trips

---
+++ db/migrate/20181004222410_add_column_status_to_trips.rb
@@ -0,0 +1,5 @@
+class AddColumnStatusToTrips < ActiveRecord::Migration[5.2]
+  def change
+    add_column :trips, :completed, :boolean, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_214600) do
+ActiveRecord::Schema.define(version: 2018_10_04_222410) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_214600) do
     t.date "start_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
   create_table "users", force: :cascade do |t|

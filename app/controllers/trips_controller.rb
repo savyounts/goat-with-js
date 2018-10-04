b471c8ea80a222fe28a9811bc1d16ce983246328
github username <github email address>
2018-10-04 16:22:50 -0600

    commit

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,13 @@
 class TripsController < ApplicationController
 
   def new
+    @trip = Trip.new
   end
 
   def create
+    @trip = Trip.create(trip_params)
+    @trip.user = current_user
+    redirect_to trip_path(@trip)
   end
 
   def edit

63f89884dc50a062bcfc78126b0d0b9087308081
github username <github email address>
2018-10-04 14:15:47 -0600

    create sign up form and user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render new
+    end
   end
 
   def show
+    @user = User.find_by(params[:id]) if logged_in?
   end
 
 end

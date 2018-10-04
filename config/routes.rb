26fb1eab60baac0c385c9ec7d20d7fd770abec14
github username <github email address>
2018-10-04 13:57:54 -0600

    add welcome controller, session controller, and all view templates

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :trips
   resources :destinations
   resources :users
+
+  root to: 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

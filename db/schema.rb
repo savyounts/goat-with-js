badacbb30ce064de23224cf4742f928b09a743d7
github username <github email address>
2018-10-05 14:50:49 -0600

    generate comment model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_222410) do
+ActiveRecord::Schema.define(version: 2018_10_05_205010) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "destination_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"

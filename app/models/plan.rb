6ce4b5c8e6cd2a23e19b08137519327076964fa2
github username <github email address>
2018-10-06 17:18:22 -0600

    fix destinations to associate to new or current trips

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,8 +2,10 @@ class Plan < ApplicationRecord
   belongs_to :trip
   belongs_to :destination
 
-  accepts_nested_attributes_for :trip
+  accepts_nested_attributes_for :trip, reject_if: :reject_trip
 
-
-
+  def reject_trip(attributes)
+    attributes['name'].blank?
+  end
+  
 end

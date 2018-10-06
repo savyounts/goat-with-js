6232de362adfe89200b8c9eb4dc657544994bd95
github username <github email address>
2018-10-05 18:05:15 -0600

    set up omniauth in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # if request.env["onmiauth.auth"]
-    #   @user = User.find_or_create_by_omniauth(auth)
-    #   session[:user_id] = @user.id
-    #   redirect_to user_path(@user)
-    # else
+    if request.env["onmiauth.auth"]
+      @user = User.find_or_create_by(email: auth["info"]['email']) do |u|
+         u.username = auth['info']['name']
+         u.email = auth['info']['email']
+         u.pasword = SecureRandom.hex
+      end
+
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
       @user = User.find_by(username: params[:username])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
@@ -16,7 +21,7 @@ class SessionsController < ApplicationController
       else
         redirect_to '/login'
       end
-    # end
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,7 @@ Rails.application.routes.draw do
 
   get '/login', :to => 'sessions#new', :as => :login
   get '/logout', to: 'sessions#destroy'
-  get '/auth/:provider/callback', :to => 'sessions#create'
-
+  get '/auth/facebook/callback' => 'sessions#create'
 
   root to: 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

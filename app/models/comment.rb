ff96692d3e1af8831b22177aa57c91894586a23d
github username <github email address>
2018-10-08 11:50:22 -0600

    oder comments by amount of likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :destination_id, :user_id, :content, presence: true
+  scope :top_comment, -> { order("likes desc") }
 
   def like
     self.likes += 1

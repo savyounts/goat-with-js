891d4ff19e8c876c44f89c08b11ca2d1deca12a5
github username <github email address>
2018-10-08 15:57:18 -0600

    refactor user show view into content partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def conditional_content
+    if @current_user.trips.empty?
+      content_tag(:p, "Looks like you don't have any trips yet.")
+    else
+      render 'content'
+    end
+  end
+  
 end

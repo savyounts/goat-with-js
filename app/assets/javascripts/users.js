ccb25811160b9e659c504887c39cf4c1b8207d56
github username <github email address>
2018-10-30 12:49:14 -0600

    add js for even when clicking my trips to display only my destinations button and create trip button

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,11 +5,26 @@ $( document ).ready(function() {
     let id = $('#my-destinations').attr("data-id")
     $.get(`/users/${id}.json`, (user) =>{
       $('#user-show-header').text("My destinations.")
-      $('#user-show-button').attr("href", `/destinations/new`)
-      $('#user-show-button').text("Create new destination.")
+      let myTrips = `<a class= "button" href="#" id="my-trips" data-id="${id}">my trips.</a>`
+      let createDestination = `<a class= "button create_button" href="/destinations/new">Create a new destination.</a>`
+      $('#user-buttons').html(myTrips + createDestination)
+      // $('#user-content').html(create handlebar template)
 
     })
     })
+
+    $('#my-trips').on('click', (e) =>{
+      e.preventDefault()
+      let id = $('#my-trips').attr("data-id")
+      $.get(`/users/${id}.json`, (user) =>{
+        $('#user-show-header').text("My trips.")
+        let myDestinations = `<a class= "button" href="#" id="my-destinations" data-id="${id}">my destinations.</a>`
+        let createTrip = `<a class= "button create_button" href="/users/${id}/trips/new">Create a new trip.</a>`
+        $('#user-buttons').html(myDestinations + createTrip)
+        // $('#user-content').html(create handlebar template)
+
+      })
+      })
 
 
 

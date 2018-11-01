4cfde0076f22b414d3b51d9f942354308e7b7fa0
github username <github email address>
2018-11-01 13:01:36 -0600

    change info <p> tags to <span> on destination content partial

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,40 +9,40 @@ $( document ).ready(function() {
     return `/images/${Math.floor((Math.random()*7)+1)}.jpg`
   })
 
-// click my destinations
-  $('#user-buttons').on('click', '.my-destinations',  (e) =>{
-    e.preventDefault()
-    updatePage('destination', 'trips', 'custom_destination')
-  })
+// update helpers
+  const updatePage = (object, other, custom) => {
+    let id = $(`.my-${object}s`).attr("data-id")
+    $.get(`/users/${id}.json`, (user) =>{
+      $('#user-show-header').text(`My ${object}s.`)
 
-// click my trips
+      // buttons
+      let createTrip = createButton(id, object)
+      $('#user-buttons').html(displayOtherButton(id, other) + createTrip)
+
+      // content
+      let contentHtml = HandlebarsTemplates[`my-${object}s-template`]({object: user[`${custom || object}s`]})
+      $('#user-content').html(contentHtml)
+    })
+  }
+
+  const createButton = (id, object) => {
+    return `<a class= "button create_button" href="/users/${id}/${object}s/new">Create a new ${object}.</a>`
+  }
+
+  const displayOtherButton = (id, other) =>{
+    return `<a class= "button my-${other}" href="#" data-id="${id}">my ${other}.</a>`
+  }
+
+  // event handlers
+    $('#user-buttons').on('click', '.my-destinations',  (e) =>{
+      e.preventDefault()
+      updatePage('destination', 'trips', 'custom_destination')
+    })
+
     $('#user-buttons').on('click', '.my-trips', (e) =>{
       e.preventDefault()
       updatePage('trip', 'destinations')
     })
 
-const updatePage = (object, other, custom) => {
-  let id = $(`.my-${object}s`).attr("data-id")
-  $.get(`/users/${id}.json`, (user) =>{
-    $('#user-show-header').text(`My ${object}s.`)
-
-    // buttons
-    let createTrip = createButton(id, object)
-    $('#user-buttons').html(displayOtherButton(id, other) + createTrip)
-
-    // content
-    let contentHtml = HandlebarsTemplates[`my-${object}s-template`]({object: user[`${custom || object}s`]})
-    $('#user-content').html(contentHtml)
-  })
-}
-
-
-const createButton = (id, object) => {
-  return `<a class= "button create_button" href="/users/${id}/${object}s/new">Create a new ${object}.</a>`
-}
-
-const displayOtherButton = (id, other) =>{
-  return `<a class= "button my-${other}" href="#" data-id="${id}">my ${other}.</a>`
-}
 
 })

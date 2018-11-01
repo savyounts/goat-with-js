b2918735c911f7967cac715dca9527b2880f4464
github username <github email address>
2018-11-01 12:06:12 -0600

    refactor destination.js to split up event handlers into smaller functions

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,70 +1,80 @@
 $( document ).ready(function() {
 
-  $('#comment-form').hide()
-
-  const hideButtons = () => {
+  // initial page set up
+  const hideElements = () => {
     let next = $('#next-button')
     let previous = $('#previous-button')
     if (previous.attr("data-destId") === "1") {previous.hide()}
     if (next.attr("data-last-dest") === next.attr("data-destId")) {next.hide()}
+    $('#comment-form').hide()
   }
-  hideButtons()
+  hideElements()
 
-  $('.comments').on('click', '#next-button', (e) => {
+  const conditionalButtons = (creatorId = null) => {
+    let creator = creatorId || parseInt($('#user').attr("data-creator_id"))
+    let user = parseInt($('#user').attr("data-user_id"))
+    let $conditionals = $('#conditional-buttons')
+    user !== creator ? $conditionals.hide() : $conditionals.show()
+  }
+  conditionalButtons()
+
+ // content updaters
+  const updateButtonLinks = (destinationId) => {
+    $('#next-button').show().attr("data-destId", destinationId)
+    $('#previous-button').show().attr("data-destId", destinationId)
+    $('#add').attr("href", `/destinations/${destinationId}/plans/new`)
+    $('#delete').attr("href", `/destinations/${destinationId}`)
+    $('#update').attr("href", `/destinations/${destinationId}/edit`)
+    $('.submit-comment').attr("data-destination_id", destinationId)
+  }
+
+  const updateDestinationContent = (destination) =>{
+    $('#dest-name').text(destination.name)
+    $('#location').text(`Location: ${destination.city}, ${destination.country}`)
+    $('#description').text(destination.description)
+
+    let md = destination.must_dos ? destination.must_dos : ""
+    $('#must_dos').text(md)
+
+    let dd = destination.dont_dos ? destination.dont_dos : ""
+    $('#dont_dos').text(dd)
+
+    let dt = destination.day_trips ? destination.day_trips : ""
+    $('#day_trips').text(dt)
+}
+
+const updateComments = (destination) => {
+  $('.comment-div').html("")
+  destination.comments.forEach(comment => {
+    let new_comment = HandlebarsTemplates['comments-template']({comment: comment})
+    $('.comment-div').append(new_comment)
+  })
+}
+
+  const updatePage = (destinationId) =>{
+    let current_user_id = parseInt($('#user').attr("data-user_id"))
+    $.get(`/destinations/${destinationId}.json`, (destination) =>{
+      updateDestinationContent(destination)
+      updateButtonLinks(destinationId)
+      hideElements()
+      $('.show-form').show()
+      conditionalButtons(destination.creator_id)
+      updateComments(destination)
+    })
+  }
+
+  // event handlers
+  $('.wrapper').on('click', '#next-button', (e) => {
     e.preventDefault()
     let nextId = parseInt($("#next-button").attr("data-destId")) + 1
     updatePage(nextId)
   })
 
-  $('.comments').on('click', '#previous-button', (e) => {
+  $('.wrapper').on('click', '#previous-button', (e) => {
     e.preventDefault()
     let previousId = parseInt($("#previous-button").attr("data-destId")) - 1
     updatePage(previousId)
   })
-
-  const updatePage = (destinationId) =>{
-    let current_user_id = parseInt($('#user').attr("data-user_id"))
-    $.get(`/destinations/${destinationId}.json`, (destination) =>{
-      // destination content
-      $('#dest-name').text(destination['name'])
-      $('#location').text(`Location: ${destination['city']}, ${destination['country']}`)
-      $('#description').text(destination['description'])
-
-      let md = destination['must_dos'] ? destination['must_dos'] : ""
-      $('#must_dos').text(md)
-
-      let dd = destination['dont_dos'] ? destination['dont_dos'] : ""
-      $('#dont_dos').text(dd)
-
-      let dt = destination['day_trips'] ? destination['day_trips'] : ""
-      $('#day_trips').text(dt)
-
-      // button links
-      $('#next-button').show().attr("data-destId", destinationId)
-      $('#previous-button').show().attr("data-destId", destinationId)
-      hideButtons()
-      $('#add').attr("href", `/destinations/${destinationId}/plans/new`)
-      $('#delete').attr("href", `/destinations/${destinationId}`)
-      $('#update').attr("href", `/destinations/${destinationId}/edit`)
-      $('.submit-comment').attr("data-destination_id", destinationId)
-
-      // conditional buttons
-      if(current_user_id !== destination.creator_id){
-        $('#conditional-buttons').hide()
-      }else{
-        $('#conditional-buttons').show()
-      }
-
-
-      // Comments
-      $('.comment-div').html("")
-      destination['comments'].forEach(comment =>{
-        // let comment.username =
-        let new_comment = HandlebarsTemplates['comments-template']({comment: comment})
-        $('.comment-div').append(new_comment)
-      })
-    })
-  }
 
   // show comment form
   $('.show-form').on('click', (e) =>{

114d4fb341ef44a04ca571a59ff4c5df2ced8b50
github username <github email address>
2018-11-01 10:35:32 -0600

    refactor like/dislike functions on comment.js

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,101 +1,71 @@
 $( document ).ready(function() {
   // Comment class
-  const store = {comments:[]}
   class Comment{
     constructor(data){
       this.id = data.id
       this.content = data.content
       this.likes = 0
-      store.comments.push(this)
     }
 
     toString(){
       return HandlebarsTemplates['comments-template']({comment: this})
     }
-
-    add_like(){
-      return ++this.likes
-    }
-
-    dislike(){
-      if (this.number > 0){
-        --this.likes
-      }
-      return this.likes
-    }
   }
 
 // like/dislike functions
-
-function findComment(id){
-  return store.comments.find(comment => comment['id'] === id)
-}
-
-$('.comment-div').on('click', '.like-button', (e)=>{
-  e.preventDefault()
-  let info = $('.like-button').data()
-  let commentId = info.commentid
-  let likes = info.likes + 1
-  let destinationId = info.destination
-  let data = {comment:{
-      likes: likes
-    }
+  const updateLikes = (buttonClass, number) =>{
+      e.preventDefault()
+      let info = $(buttonClass).data()
+      let commentId = info.commentid
+      let likes = info.likes + number
+      let destinationId = info.destination
+      let data = {comment:{
+          likes: likes
+        }
+      }
+      let update = $.ajax({
+              url: `/comments/${commentId}`,
+              type: 'PATCH',
+              dataType: "json",
+              data: data
+            });
+      update.done(function(data){
+        $(`#comment-${commentId}-likes`).html(likes)
+      })
   }
-  let update = $.ajax({
-          url: `/comments/${commentId}`,
-          type: 'PATCH',
-          dataType: "json",
-          data: data
-        });
-  update.done(function(data){
-    $(`#comment-${commentId}-likes`).html(likes)
+
+  $('.comment-div').on('click', '.like-button', (e)=>{
+    updateLikes('.like-button', 1)
   })
-})
 
-$('.comment-div').on('click', '.dislike-button', (e)=>{
-  e.preventDefault()
-  let info = $('.dislike-button').data()
-  let commentId = info.commentid
-  let likes = info.likes - 1
-  let destinationId = info.destination
-  let data = {comment:{
-      likes: likes
-    }
-  }
-  let update = $.ajax({
-          url: `/comments/${commentId}`,
-          type: 'PATCH',
-          dataType: "json",
-          data: data
-        });
-  update.done(function(data){
-    $(`#comment-${commentId}-likes`).html(likes)
+  $('.comment-div').on('click', '.dislike-button', (e)=>{
+    updateLikes('.like-button', -1)
   })
-})
 
 
-// submiting a comment
+  // submiting a comment
 
-$('.submit-comment').on('click', (e) => {
-  e.preventDefault()
-  let $button = $('.submit-comment');
-  let data = {comment: {
-        'action' : '/comments',
-        'user_id': $button.data("user_id"),
-        'destination_id': $button.data("destination_id"),
-        'content' : $( '.comment-textarea' ).val()
-    }
-  };
-  $('.comment-textarea').val('').attr('placeholder', "leave message here")
-  let posting = $.post('/comments', data);
+  $('.submit-comment').on('click', (e) => {
+    e.preventDefault()
+    let $button = $('.submit-comment');
+    let data = {comment: {
+          'action' : '/comments',
+          'user_id': $button.data("user_id"),
+          'destination_id': $button.data("destination_id"),
+          'content' : $( '.comment-textarea' ).val()
+      }
+    };
 
-  posting.done(function(data) {
-    let new_comment = new Comment(data)
-    $('.comment-div').append(new_comment.toString())
-    $('#comment-form').hide()
-    $('.show-form').show()
-    console.log(new_comment.toString())
-  });
-})
+    let posting = $.post('/comments', data);
+
+    posting.done(function(data) {
+      let new_comment = new Comment(data)
+      $('.comment-div').append(new_comment.toString())
+      $('.comment-textarea').val('').attr('placeholder', "leave message here")
+      $('#comment-form').hide()
+      $('.show-form').show()
+      console.log(new_comment.toString())
+    });
+  })
 
 })
